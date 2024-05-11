0edf7c83c5c5d7f4c6981015dc8de83faded3061
Kuldeep Sharma <mrksphp@gmail.com>
2024-05-11 22:18:39 +0530

    add: initial commit

---
+++ android/app/src/main/java/ion/cap/background/api/notification/MainActivity.java
@@ -0,0 +1,5 @@
+package ion.cap.background.api.notification;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ion.cap.background.api.notification',
+  appName: 'ion-cap-background-api-notification',
+  webDir: 'www'
+};
+
+export default config;
